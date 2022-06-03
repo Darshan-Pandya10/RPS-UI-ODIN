4147752e536f5dee5cd67bcf39daa5adfad05c40
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-06-03 18:20:34 +0530

    [ADD] Correct logic for final winner

--- test.js
+++ test.js
@@ -13,9 +13,9 @@ const scissor = document.querySelector('#scissor');
 let playerPoint = document.querySelector(".player-score");
 let computerPoint = document.querySelector(".computer-score");
 
-gameRound = 1;
-playerScore = 1;
-computerScore = 1;
+let gameRound = 1;
+let playerScore = 0;
+let computerScore = 0;
 
 const finalResult = document.querySelector('.final-result')
 
@@ -28,10 +28,8 @@ let Options = ["rock", "paper", "scissor"];
 
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    for(i = gameRound ; i <= 5 ; i++) {
     result.classList.add('result-show')
     playerSelection = choice.getAttribute("id");
-    console.log(playerSelection);
     result.classList.add('.result-show')
 
     pChoice.textContent = `Player Choice : ${playerSelection}`;
@@ -51,99 +49,102 @@ choices.forEach((choice) => {
 
       if (playerSelection === "rock" && computerSelection === "rock") {
         result.textContent = "Game is tie!";
-      } else if (playerSelection === "paper" && computerSelection === "paper") {
+      } 
+      
+      else if (playerSelection === "paper" && computerSelection === "paper") {
         result.textContent = "Game is tie!";
-      } else if (
-        playerSelection === "scissor" &&
-        computerSelection === "scissor"
-      ) {
+      }   
+      
+      else if (
+        playerSelection === "scissor" && computerSelection === "scissor") {
         result.textContent = "Game is tie!";
-      } else if (playerSelection === "rock" && computerSelection === "paper") {
+      } 
+      
+      else if (playerSelection === "rock" && computerSelection === "paper") {
         result.textContent = "You Lose! Paper beats rock.";
-        computerPoint.textContent = `Computer Point : ${computerScore++}`;
-      } else if (
-        playerSelection === "rock" &&
-        computerSelection === "scissor"
-      ) {
+        computerPoint.textContent = `Computer Point : ${++computerScore}`;
+        
+      } 
+      
+      else if (
+        playerSelection === "rock" && computerSelection === "scissor") {
         result.textContent = "You win! rock beats scissor.";
-        playerPoint.textContent = `Player Point : ${playerScore++}`;
-      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerPoint.textContent = `Player Point : ${++playerScore}`;
+        
+      }
+       
+      else if (playerSelection === "paper" && computerSelection === "rock") {
         result.textContent = "You win! Paper beats rock.";
-        playerPoint.textContent = `Player Point : ${playerScore++}`;
-      } else if (
-        playerSelection === "paper" &&
-        computerSelection === "scissor"
-      ) {
+        playerPoint.textContent = `Player Point : ${++playerScore}`;
+        
+      } 
+      
+      else if (
+        playerSelection === "paper" && computerSelection === "scissor") {
         result.textContent = "You Lose! scissor beats paper.";
-        computerPoint.textContent = `Computer Point : ${computerScore++}`;
-      } else if (
-        playerSelection === "scissor" &&
-        computerSelection === "rock"
-      ) {
+        computerPoint.textContent = `Computer Point : ${++computerScore}`;
+        
+      } 
+      
+      else if (
+        playerSelection === "scissor" && computerSelection === "rock") {
         result.textContent = "You Lose! rock beats scissor.";
-        computerPoint.textContent = `Computer Point : ${computerScore++}`;
-      } else if (
-        playerSelection === "scissor" &&
-        computerSelection === "paper"
-      ) {
+        computerPoint.textContent = `Computer Point : ${++computerScore}`;
+        
+      } 
+      
+      else if (
+        playerSelection === "scissor" && computerSelection === "paper") {
         result.textContent = "You win! scissor beats paper.";
-        playerPoint.textContent = `Player Point : ${playerScore++}`;
-      } else {
-        result.textContent = "Please Choose your input currectly!";
+        playerPoint.textContent = `Player Point : ${++playerScore}`;
+        
+      }
+
+      if(playerScore === 5 || computerScore === 5){
+    
+        if (playerScore > computerScore){
+          finalResult.textContent = "Congrats! You are a Winner";
+        }
+      
+        else if (playerScore < computerScore){
+          finalResult.textContent = "You Lost the Game, Better Luck Next Time.";
+        }
+  
+        else if (playerScore === computerScore){
+          finalResult.textContent = "game is tie!";
+        }
+  
+        finalResult.classList.add('final-result-show')
+        rock.classList.add('choice-decline')
+        paper.classList.add('choice-decline')
+        scissor.classList.add('choice-decline')
+        
       }
      
     }
-    if(gameRound == 5){
-      finalResult.classList.add('final-result-show')
-      rock.classList.add('choice-decline')
-      paper.classList.add('choice-decline')
-      scissor.classList.add('choice-decline')
-
-
-      
     
-      if (playerScore > computerScore){
-        finalResult.textContent = 'Congrats! You are a Winner';
-      }
-    
-      else if (playerScore < computerScore){
-        finalResult.textContent = 'You Lost the Game, Better Luck Next Time.'
-      }
 
-      else if (playerScore == computerScore){
-        finalResult.textContent = 'game is tie!';
-      }
 
-      else{
-        finalResult.textContent = 'Something went wrong!'
-      }
-      
-    }
-
-    else {
-      finalResult.textContent = 'Something went wrong!'
-    }
-
-  }
   
   
-
     playGame();
 
-  });
-});
+  })
+})
 
-function Restart() {
-gameRound = 0;
-playerScore = 0;
-computerScore = 0;
+// function Restart() {
+// gameRound = 0;
+// playerScore = 0;
+// computerScore = 0;
 
-finalResult.textContent = " ";
-result.textContent = " ";
-pChoice.textContent = " Player Choice : Click on one of this tile  ";
-cChoice.textContent = " ";
+// finalResult.textContent = " ";
+// result.textContent = " ";
+// pChoice.textContent = " Player Choice : Click on one of this tile  ";
+// cChoice.textContent = " ";
 
-}
+
+
+// }
 
 
 
